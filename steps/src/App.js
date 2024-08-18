e09154b7113614b4fbc7d03f6112dc3fd9f846f9
stanovljanac <sebekmihailo@gmail.com>
2024-08-19 00:57:00 +0200

    Started new small app for trip items

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,64 +1,163 @@
 import { useState } from "react";
+import "./indexFarAway.css";
 
-const messages = [
-  "Learn React ⚛️",
-  "Apply for jobs 💼",
-  "Invest your new income 🤑",
+// Trip Away app
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: true },
 ];
+
 export default function App() {
   return (
-    <div>
-      <Steps></Steps>
-      <Steps></Steps>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
 
-function Steps() {
-  // Create state variable
-  const [step, setStep] = useState(1);
-  const [isOpen, setIsOpen] = useState(true);
+function Logo() {
+  return <h1>Far away</h1>;
+}
 
-  function handlePrevious() {
-    if (step > 1) setStep((step) => step - 1);
-  }
-  function handleNext() {
-    if (step < 3) setStep((step) => step + 1);
+function Form() {
+  // Create a piece of state
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    setDescription("");
+    setQuantity(1);
   }
 
   return (
-    <div>
-      <button className="close" onClick={() => setIsOpen((isOpen) => !isOpen)}>
-        &times;
-      </button>
-      {isOpen && (
-        <div className="steps">
-          <div className="numbers">
-            <div className={step >= 1 ? "active" : ""}>1</div>
-            <div className={step >= 2 ? "active" : ""}>2</div>
-            <div className={step >= 3 ? "active" : ""}>3</div>
-          </div>
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your trip</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        // Take the value of state
+        value={description}
+        // Changing the state
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
 
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
-
-          <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handlePrevious}
-            >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handleNext}
-            >
-              Next
-            </button>
-          </div>
-        </div>
-      )}
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
     </div>
   );
 }
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button></button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have x items on your list, and you aalready packed Y(X%)</em>
+    </footer>
+  );
+}
+
+// Steps app
+
+// const messages = [
+//   "Learn React ⚛️",
+//   "Apply for jobs 💼",
+//   "Invest your new income 🤑",
+// ];
+// export default function App() {
+//   return (
+//     <div>
+//       <Steps></Steps>
+//       <Steps></Steps>
+//     </div>
+//   );
+// }
+
+// function Steps() {
+//   // Create state variable
+//   const [step, setStep] = useState(1);
+//   const [isOpen, setIsOpen] = useState(true);
+
+//   function handlePrevious() {
+//     if (step > 1) setStep((step) => step - 1);
+//   }
+//   function handleNext() {
+//     if (step < 3) setStep((step) => step + 1);
+//   }
+
+//   return (
+//     <div>
+//       <button className="close" onClick={() => setIsOpen((isOpen) => !isOpen)}>
+//         &times;
+//       </button>
+//       {isOpen && (
+//         <div className="steps">
+//           <div className="numbers">
+//             <div className={step >= 1 ? "active" : ""}>1</div>
+//             <div className={step >= 2 ? "active" : ""}>2</div>
+//             <div className={step >= 3 ? "active" : ""}>3</div>
+//           </div>
+
+//           <p className="message">
+//             Step {step}: {messages[step - 1]}
+//           </p>
+
+//           <div className="buttons">
+//             <button
+//               style={{ backgroundColor: "#7950f2", color: "#fff" }}
+//               onClick={handlePrevious}
+//             >
+//               Previous
+//             </button>
+//             <button
+//               style={{ backgroundColor: "#7950f2", color: "#fff" }}
+//               onClick={handleNext}
+//             >
+//               Next
+//             </button>
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
