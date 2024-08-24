4e60f46278bd10becd50f24d044d5b90a548b080
stanovljanac <sebekmihailo@gmail.com>
2024-08-24 18:33:11 +0200

    Initial commit for new react quiz project

---
+++ quiz/src/App.js
@@ -0,0 +1,10 @@
+// import DateCounter from "./DateCounter";
+import Header from "./Header";
+
+export default function App() {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+}
---
+++ quiz/src/Main.js
@@ -0,0 +1,10 @@
+function Main() {
+  return (
+    <main className="main">
+      <p>1/15</p>
+      <p>Question</p>
+    </main>
+  );
+}
+
+export default Main;
