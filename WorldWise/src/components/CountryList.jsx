4196f4e5b9169d0ddd6cb221b92471eaf0b08c25
stanovljanac <sebekmihailo@gmail.com>
2024-08-26 01:37:39 +0200

    Added state to URL and Programmatic Navigation for pages

---
+++ WorldWise/src/components/CountryList.jsx
@@ -0,0 +1,28 @@
+import CountryItem from "./CountryItem";
+import styles from "./CountryList.module.css";
+import Message from "./Message";
+import Spinner from "./Spinner";
+
+function CountryList({ cities, isLoading }) {
+  const countries = cities.reduce((arr, city) => {
+    if (arr.map((el) => el.country).includes(city.country)) return arr;
+    return [...arr, { country: city.country, emoji: city.emoji }];
+  }, []);
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking a city on a map" />
+    );
+
+  if (isLoading) return <Spinner />;
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={country.country} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountryList;
